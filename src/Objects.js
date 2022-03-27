a03a8dc3b21f903cb2420287666fa0b6a4fe7d0f
Nikita Latysh <n.latysh@g.nsu.ru>
2022-03-28 01:29:57 +0700

    Задание по объектам

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,15 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data.gender == 'female') {
+        delete data.age;
+    } else if (data.gender == 'male') {
+        if (!('income' in data)) {
+            data.income = 100000;
+        }
+    }
+
+    return data;
 }
 
 /*
@@ -15,6 +24,12 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    const bigObj = { ...obj1, ...obj2, ...obj3 };
+    let keys = Object.keys(bigObj);
+
+    keys.sort();
+
+    return keys;
 }
 
 /*
@@ -23,4 +38,13 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let objArray = [];
+
+    for (let i = 0; i < count; i++) {
+        objArray[i] = JSON.parse(
+            `{ "id": ${i}, ` + JSON.stringify(obj).substring(1),
+        );
+    }
+
+    return objArray;
 }
